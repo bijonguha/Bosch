d1728de2c752c01b2b0981a59fd4f1dcff5117a6
bijonguha <iitbguha@gmail.com>
2019-08-22 19:03:06 +0530

    new functionalities tested : segmentation

--- intel_ocr/test.py
+++ intel_ocr/test.py
@@ -118,3 +118,78 @@ for left,right in zip(uppers[diff_index], lowers[diff_index]):
      cv2.rectangle(rotated ,(0,left),(W,right),(0,255,0),3)
 
 cv2.imwrite("result_rect.png", rotated)
+
+#%%
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread("data/bijon.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+# noise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+# sure background area
+sure_bg = cv2.dilate(opening,kernel,iterations=3)
+cv2.imshow('img',sure_bg)
+# Finding sure foreground area
+dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+cv2.imshow('img',sure_fg)
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg,sure_fg)
+
+# Marker labelling
+ret, markers = cv2.connectedComponents(sure_bg)
+
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+
+markers = cv2.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+
+cv2.imshow('image', markers)
+cv2.imwrite('image.jpg',img)
+#%%
+
+import cv2
+import numpy as np
+
+# Load the image
+img = cv2.imread("data/image_line_2.jpg")
+
+# convert to grayscale
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# smooth the image to avoid noises
+gray = cv2.medianBlur(gray,5)
+
+# Apply adaptive threshold
+ret, thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV)
+thresh_color = cv2.cvtColor(thresh,cv2.COLOR_GRAY2BGR)
+
+## apply some dilation and erosion to join the gaps
+thresh = cv2.dilate(thresh,None,iterations = 3)
+#thresh = cv2.erode(thresh,None,iterations = 2)
+
+# Find the contours
+contours,hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+# For each contour, find the bounding rectangle and draw it
+for cnt in contours:
+    x,y,w,h = cv2.boundingRect(cnt)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.rectangle(thresh_color,(x,y),(x+w,y+h),(0,255,0),2)
+
+# Finally show the image
+cv2.imshow('img',img)
+cv2.imshow('res',thresh_color)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
