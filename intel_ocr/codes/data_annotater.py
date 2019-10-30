01aea2d11e0f7cce8f319a8d3c13fbf5622a69ea
bijonguha <iitbguha@gmail.com>
2019-10-30 16:09:02 +0530

    replaced exponential ^ by ** now, added val of each line as a comun

--- intel_ocr/codes/data_annotater.py
+++ intel_ocr/codes/data_annotater.py
@@ -6,6 +6,37 @@ Created on Fri Oct 25 15:19:33 2019
 """
 #%%
 import pandas as pd
+import ast
+import operator as op
+
+'''
+Evaluatore new
+'''
+# supported operators
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
+             ast.USub: op.neg}
+
+def eval_expr(expr):
+    """
+    >>> eval_expr('2^6')
+    4
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4^5) / (6 + -7)')
+    -5.0
+    """
+    return eval_(ast.parse(expr, mode='eval').body)
+
+def eval_(node):
+    if isinstance(node, ast.Num): # <number>
+        return node.n
+    elif isinstance(node, ast.BinOp): # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp): # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
 
 img_name = input('Please enter name of image : ')
 boxes = int(input('Please enter number of workspaces : '))
@@ -14,23 +45,31 @@ box_num = []
 line_name = []
 char = []
 exp =[]
+line_val = []
 
 for i in range(boxes):
     lines = input('Please enter lines of workspace %d separated by ; (semicolon)\n' %(i+1) )
     lines = lines.split(';')
     for j,line in enumerate(lines):
+
         flag = 0
-        for k in range(len(line)):
-            if(line[k] == '^'):
-                flag = 1
-                continue
+        k = 0
+        val = eval_expr(line)
+        while( k < len(line) ):
+            if(line[k] == '*'):
+                if(line[k+1] == '*'):
+                    flag = 1
+                    k = k+2
+                    continue
             box_num.append(i)
             line_name.append('%d%d' %(i,j))
             char.append(line[k])
             exp.append(flag)
+            line_val.append(val)
             flag = 0
-
-data_dict = {'box_num': box_num, 'line_name': line_name, 'char': char, 'exp':exp}  
+            k = k+1
+        
+data_dict = {'box_num': box_num, 'line_name': line_name, 'char': char, 'exp':exp, 'line_val':line_val}  
 df_chars = pd.DataFrame(data=data_dict, index=None)           
 
 try:
