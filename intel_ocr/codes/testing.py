d0cd92b7e63ad68ecdedb70b964a5debf02f0b62
bijonguha <iitbguha@gmail.com>
2019-11-17 16:36:26 +0530

    made inline with utils.py updated

--- intel_ocr/codes/testing.py
+++ intel_ocr/codes/testing.py
@@ -12,6 +12,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 from scipy import ndimage
 import math
 import keras
@@ -32,8 +33,8 @@ try:
     model = keras.models.load_model('models/DCNN_10AD_sy.h5')
 except:
     print('Model couldnot be loaded')
-
-def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+#%%
+def image_resize(image, width = None, height = None, inter = cv2.INTER_LINEAR):
     # initialize the dimensions of the image to be resized and
     # grab the image size
     dim = None
@@ -161,7 +162,7 @@ def shift(img,sx,sy):
     return shifted
 
 
-def predict(img,x1,y1,x2,y2,model):
+def predict(img,x1,y1,x2,y2,model,proba = False):
     '''
     predict  : Function to predict the character
     argument:
@@ -173,6 +174,36 @@ def predict(img,x1,y1,x2,y2,model):
     
     '''
     gray = img[y1:y2, x1:x2]
+    
+    # Steps to remove noises in image due to cropping
+    temp = gray.copy()
+    
+    kernel_temp = np.ones((3,3), np.uint8) 
+    temp_tmp = cv2.dilate(temp, kernel_temp, iterations=3)
+    
+    # Find the contours -  To check whether its disjoint character or noise
+    if(cv2.__version__ == '3.3.1'): 
+        xyz,contours_tmp,hierarchy = cv2.findContours(temp_tmp,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    else:
+        contours_tmp,hierarchy = cv2.findContours(temp_tmp,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        
+    if(len(contours_tmp) > 1):
+        # Find the contours
+        if(cv2.__version__ == '3.3.1'): 
+            xyz,contours,hierarchy = cv2.findContours(gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        else:
+            contours,hierarchy = cv2.findContours(gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        #Creating a mask of only zeros  
+        mask = np.ones(gray.shape[:2], dtype="uint8") * 0
+        #print('Yes')
+        # Find the index of the largest contour
+        areas = [cv2.contourArea(c) for c in contours]
+        max_index = np.argmax(areas)
+        cnt=contours[max_index]
+        
+        cv2.drawContours(mask, [cnt], -1, 255, -1)
+        #Drawing those contours which are noises and then taking bitwise and
+        gray = cv2.bitwise_and(temp, temp, mask=mask)
     
     # Image Preprocessing
     kernel = np.ones((1,1), np.uint8) 
@@ -220,11 +251,19 @@ def predict(img,x1,y1,x2,y2,model):
     gray = gray/255
     # Prediction
     classes = model.predict(gray, batch_size=2)
-    index = np.argmax(classes[0])  
-    c = ['0','1','2','3','4','5','6','7','8','9','+','-','*','*(',')']
-
-#    print(c[index])
-    return c[index]
+    ind1,ind2 = np.argpartition(classes[0], -2)[-2:]
+    c = ['0','1','2','3','4','5','6','7','8','9','+','-','*','(',')']
+    
+    if(c[ind1] == '2' and c[ind2] == '9' and classes[0][ind1] > 0.40):
+        return c['ind1']
+    
+    #if(c[ind2] == '9'):
+    #print(c[ind1],c[ind2], classes[0][ind1], classes[0][ind2])
+    
+    if (proba == True):
+        return classes[0][ind2]
+    
+    return c[ind2]
 
 def extract_box(img, show=True):
     '''
@@ -285,7 +324,18 @@ def extract_box(img, show=True):
     #finding out locations of rectangle
     for cnt in workspace_contours:
         x,y,w,h = cv2.boundingRect(cnt)
-        rectangle_locs.append([x,y,w,h])
+        rectangle_locs.append([x,y,w,h])        
+    
+    if (show):
+        fig = plt.figure(figsize=(7,9))
+        fig.suptitle('Extracted Workspaces')
+        i=1
+        l = len(rectangle_locs)
+        for rect in rectangle_locs:
+            ax = fig.add_subplot(l,1,i)
+            ax.imshow(img[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]])
+            i = i+1
+        plt.show()
         
     return rectangle_locs
 
@@ -298,6 +348,10 @@ def find_good_contours_thres(conts, alpha = 0.002):
     Function to find threshold of good contours on basis of 10% of maximum area
     Input: Contours, threshold for removing noises
     Output: Contour area threshold
+    
+    For image dim 3307*4676
+    alpha(text_segment) = 0.01
+    alpha(extract_line) = 0.002
     '''
     #Calculating areas of contours and appending them to a list
     areas = []
@@ -309,7 +363,7 @@ def find_good_contours_thres(conts, alpha = 0.002):
     
     return thres
 
-def extract_line(image, beta=0.7, show = True):
+def extract_line(image, beta=0.7, alpha=0.002, show = True):
     '''
     Function to extracts the line from the image   
     Assumption : Sufficient gap b/w lines
@@ -317,6 +371,7 @@ def extract_line(image, beta=0.7, show = True):
     argument:
         img (array): image array
         beta (0-1) : Parameter to differentiate line
+        alpha (0-1) : Parameter to select good contours
         show(bool) : to show figures or not
     output:
         uppers[diff_index]  : Upper points (x,y)
@@ -350,7 +405,7 @@ def extract_line(image, beta=0.7, show = True):
     else:
         contours,hierarchy = cv2.findContours(dilation,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         
-    cont_thresh = find_good_contours_thres(contours)
+    cont_thresh = find_good_contours_thres(contours, alpha=alpha)
 
     #Creating a mask of only ones    
     mask = np.ones(dilation.shape[:2], dtype="uint8") * 255
@@ -363,9 +418,9 @@ def extract_line(image, beta=0.7, show = True):
     cleaned_img = cv2.bitwise_and(temp, temp, mask=mask)
     
     #Dilating the cleaned image for better detection of line in cases where
-    #exponents are little up then line
+    #exponents are little up the line
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    dil_cleaned_img = cv2.dilate(cleaned_img,kernel,iterations = 5)
+    dil_cleaned_img = cv2.dilate(cleaned_img,kernel,iterations = 10)
     
     #Getting back the cleaned original image without noise
     cleaned_orig = cv2.erode(cleaned_img, kernel, iterations=1) 
@@ -384,24 +439,26 @@ def extract_line(image, beta=0.7, show = True):
     uppers = uppers[diff_index_1]
     lowers = lowers[diff_index_1]
     
-    #Extending uppers and lowers indexes to avoid cutting of chars
+    #Extending uppers and lowers indexes to avoid cutting of chars of lines
+    #Extended more uppers by 33% as exponential might lie above 
     uppers[1:] = [i-int(j)/3 for i,j in zip(uppers[1:], diff_1[1:])]
     lowers[:-1] = [i+int(j)/4 for i,j in zip(lowers[:-1], diff_1[:-1])]
     
     diff_2 = np.array([j-i for i,j in zip(uppers,lowers)])
     diff_index_2 = np.array([True]*len(uppers))
     
-    #Combining rogue exponentials into their deserving lines
+    #Combining rogue exponentials into their deserving lines. This happens when
+    #exponential and lines are separated by some distance
     for i,diff in enumerate(diff_2):
         if(i>0):
-            if( (diff_2[i-1] < (diff/2)) and (( lowers[i-1]-uppers[i]) > ((lowers[i-1]-uppers[i-1])/4)) ):
+            if( (diff_2[i-1] < (diff/2)) and (( lowers[i-1]-uppers[i]) > ((lowers[i-1]-uppers[i-1])/5)) ):
                 uppers[i] = uppers[i-1]
                 diff_2[i] = diff_2[i]+diff_2[i-1]
                 diff_index_2[i-1] = False
                 #print('Merging')
 
     diff_index = diff_index_2
-                
+    
     cleaned_orig_rec = cv2.cvtColor(cleaned_orig, cv2.COLOR_GRAY2BGR)
     
     #For changing color of intermediate lines, keeping count
@@ -452,13 +509,6 @@ def extract_line(image, beta=0.7, show = True):
     
     return cleaned_orig, uppers[diff_index], lowers[diff_index]
 
-#Y1=102
-#Y2=282
-#X1=0
-#X2=2695
-#line_name=00
-#box_num=0
-#dict_clean = dict_clean_img
 
 def evaluate(df,A,B,X,Y, ret = True):
     '''Function to evaluate mathematical equation and give bool output
@@ -470,45 +520,63 @@ def evaluate(df,A,B,X,Y, ret = True):
     #Evaluating Expression
     actual = A*X*X+(B*Y)
     
-    
     try:#If BODMAS is correct and Mathematically equation is correct
         pred = df["exp"].apply(lambda d: "**" if d==1 else "")
         pred = "".join(list(pred+df["pred"]))
-        #print("pred ",pred)
         
-        try:
-            ans = eval_expr(pred)
-        
-        except:
-            matches_left = re.findall(r'\d\(\d', pred)
-            matches_right = re.findall(r'\d\)\d', pred)
+        #looking for non digits in the start of the string for 
+        #ignoring equal to's
+        matchesN = re.findall('^\-\-', pred)
+        if(len(matchesN) > 0):
+            for s in matchesN:
+                pred = pred.replace(s,'')       
+                
             
-            for s in matches_left:
+        #looking for broken 5's
+        matches5 = re.findall(r'5\*\*-\D*', pred)
+        if(len(matches5) > 0):
+            for s in matches5:
+                sn = s.split('5**-')
+                snew = sn[0]+'5'+sn[1]
+                pred = pred.replace(s,snew)  
+
+        
+        #This except block is fired when brackets are un necessarily used 
+        #while writing the answerscripts and in strings
+        matchesB_left = re.findall(r'\d\(\d', pred)
+        matchesB_right = re.findall(r'\d\)\d', pred)
+        
+        if(len(matchesB_left) > 0 or len(matchesB_right) > 0):
+            for s in matchesB_left:
                 sn = s.split('(')
                 snew = sn[0]+'*('+sn[1]
-                pred = pred.replace(s,snew)
+                pred = pred.replace(s,snew)    
                 
-            for s in matches_right:
+            for s in matchesB_right:
                 sn = s.split(')')
                 snew = sn[0]+')*'+sn[1]
                 pred = pred.replace(s,snew) 
-                
-            ans = eval_expr(pred)
         
-        if(ans == actual):
-            val='Correct'
-        else:
-            val='Wrong'
-        #print(ans, actual, val)
+        ans = eval_expr(pred)
+#        print(pred )
+#        if(ans == actual):
+#            val='Correct'
+#        else:
+#            val='Wrong'
+#        print(ans, actual, val)
+#        if(df['pred_proba'].mean() < 0.90):
+#            return 5
+        
     except Exception as e:
-        print(e)
-        return False
+        print(pred,'-',e)
+        return 5
     
     if(ret == True):
         return actual==ans
     else:
         return ans
 
+#%%
 def text_segment(Y1,Y2,X1,X2,box_num,line_name, model, dict_clean = dict_clean_img, show = True):
     '''
     text_segment : Function to segment the characters
@@ -537,7 +605,7 @@ def text_segment(Y1,Y2,X1,X2,box_num,line_name, model, dict_clean = dict_clean_i
     else:
         contours,hierarchy = cv2.findContours(erosion,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         
-    ct_th = find_good_contours_thres(contours, alpha=0.01)
+    ct_th = find_good_contours_thres(contours, alpha=0.005)
     cnts = []
     for c in contours:       
         if( cv2.contourArea(c)**2 > ct_th):
@@ -554,28 +622,29 @@ def text_segment(Y1,Y2,X1,X2,box_num,line_name, model, dict_clean = dict_clean_i
             exp = 0
             if i+1 != len(contours_sorted):
                 x1,y1,w1,h1 = bounding_boxes[i+1]
-#                if abs(x-x1) < 20:
-#                    
-#                    minX = min(x,x1)
-#                    minY = min(y,y1)
-#                    maxX = max(x+w, x1+w1)
-#                    maxY = max(y+h, y1+h1)
-#                    x,y,x11,y11 = minX, minY, maxX, maxY
-#                    
-#                    x,y,w,h = x,y,x11-x,y11-y
-#                    i = i+1
+                if abs(x-x1) < 10 and  (h1+h) < 70:
+                    #print(h+h1)
+                    minX = min(x,x1)
+                    minY = min(y,y1)
+                    maxX = max(x+w, x1+w1)
+                    maxY = max(y+h, y1+h1)
+                    x,y,x11,y11 = minX, minY, maxX, maxY
+                    
+                    x,y,w,h = x,y,x11-x,y11-y
+                    i = i+2
+                    continue
             
             #char_locs.append([x,y,x+w,y+h])     
-            if(h<0.25*L_H and w<0.25*L_H):
+            if(h<0.10*L_H and w<0.10*L_H):
                 #print('Yes')
                 i=i+1
                 continue
-            
-            char_locs.append([x-1,y+Y1-1,x+w+1,y+h+Y1+1,w*h]) #Normalised location of char w.r.t box image
+
+            char_locs.append([x-2,y+Y1-2,x+w+1,y+h+Y1+1,w*h]) #Normalised location of char w.r.t box image
             
             cv2.rectangle(img,(x,y),(x+w,y+h),(153,180,255),2)
             if i!=0:
-                if y+h < (L_H*(1/2)) and y < bounding_boxes[i-1][1]:
+                if y+h < (L_H*(1/2)) and y < bounding_boxes[i-1][1] and h < bounding_boxes[i-1][3]:
                     exp = 1
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
             i = i+1
@@ -592,10 +661,12 @@ def text_segment(Y1,Y2,X1,X2,box_num,line_name, model, dict_clean = dict_clean_i
     df_char['exp'] = char_type
     df_char['pred'] = df_char.apply(lambda c: predict(dict_clean[box_num],c['X1'],\
            c['Y1'],c['X2'], c['Y2'],model), axis=1 )
+    df_char['pred_proba'] = df_char.apply(lambda c: predict(dict_clean[box_num],c['X1'],\
+           c['Y1'],c['X2'], c['Y2'],model, proba=True), axis=1 )
     df_char['line_name'] = line_name
     df_char['box_num'] = box_num
     return [box_num,line_name,df_char]
-
+#%%
 def checker(image_path,A=-1,B=-1,X=-1,Y=-1):
     '''
     argument:
@@ -606,13 +677,31 @@ def checker(image_path,A=-1,B=-1,X=-1,Y=-1):
     #image_path = 'data/image_24.jpg'
     #A=B=X=Y=-1
     img_i = cv2.imread(image_path)
-    img = image_resize(img_i, height = 4676, width = 3307) 
+    
+    if(type(img_i) != np.ndarray):
+        return 'Invalid Image passed'
+    #Aspect Ratio calculation
+    asp_h = img_i.shape[0]
+    asp_w = img_i.shape[1]
+    asp_ratio = round(asp_h/asp_w , 1)
+    
+    if(asp_ratio != 1.4):
+        print('Poor Image Aspect Ratio : Results Will be affected')
+    
+    if (asp_h > 4676):#Oversized image
+        img = image_resize(img_i, height = 4676, width = 3307, inter = cv2.INTER_AREA)
+    elif(asp_h < 4676):#Less sized image
+        img = image_resize(img_i, height = 4676, width = 3307, inter = cv2.INTER_LINEAR)
+        print('Image less than 300 dpi might have reduced accuracy')
+    else:
+        img = img_i
+        
     
     #Workspaces Detection
     workspaces = extract_box(img, show=False)
     
     if(len(workspaces) != 3):
-        print('Invalid worksheet image passed')
+        print('Invalid worksheet image passed, please scan properly')
         return -1
     #Defining dataframe for storing infos about every line detected
     df_lines = pd.DataFrame()
@@ -632,7 +721,7 @@ def checker(image_path,A=-1,B=-1,X=-1,Y=-1):
         df = pd.DataFrame([y1s,y2s,x1s,x2s]).transpose()
         df.columns = ['y1','y2','x1','x2']
         df['box_num'] = r
-    
+
         df_lines= pd.concat([df_lines, df])
     
         dict_clean_img.update({r:cleaned_orig})
@@ -645,7 +734,8 @@ def checker(image_path,A=-1,B=-1,X=-1,Y=-1):
     
     #df_chars contains locations of all characters along with box_num and line name
     list_chars = list(df_lines.apply(lambda row: text_segment(row['y1'],row['y2'],\
-                 row['x1'],row['x2'], row['box_num'],row['line_name'], model, show=False), axis=1))
+                 row['x1'],row['x2'], row['box_num'],row['line_name'], model, \
+                 show=False), axis=1))
     
     df_chars = pd.DataFrame(list_chars)
     df_chars.columns = ['box_num', 'line_name', 'char_df']
@@ -680,8 +770,8 @@ def checker(image_path,A=-1,B=-1,X=-1,Y=-1):
         df['char_df'].apply(lambda d: d.apply(lambda c: cv2.rectangle(box_img, \
           (c['X1'],c['Y1']),(c['X2'], c['Y2']),(255*(c['exp']==1),180,0),2+(2*c['exp'])), axis=1 ) )
         
-        df['line_status'] = df['char_df'].apply(lambda d: evaluate(d[["pred","exp"]],A,B,X,Y))
-        
+        df['line_status'] = df['char_df'].apply(lambda d: evaluate(d[["pred","exp","pred_proba"]],A,B,X,Y))
+
         df['line_val'] = df['char_df'].apply(lambda d: evaluate(d[["pred","exp"]],A,B,X,Y, ret=False))
         df_chars['line_val'][df_chars.box_num == bn] = df['line_val']
         
@@ -690,15 +780,12 @@ def checker(image_path,A=-1,B=-1,X=-1,Y=-1):
         height = int(box_img.shape[0] * scale_percent / 100)
         dim = (width, height)    
         box_img = cv2.resize(box_img, dim, interpolation = cv2.INTER_AREA)
-    #    ax = fig3.add_subplot(3,1,bn+1)
-    #    ax.axis('off')
-    #    ax.imshow(cv2.cvtColor(box_img, cv2.COLOR_BGR2RGB))
     
         #Drawing rectangle on original Image
         df_l['line_status'] = list(df['line_status']) 
         df_l.apply(lambda c: cv2.rectangle(box_img_1, (c['x1'],c['y1']),(c['x2'],\
-          c['y2']),(0,255*(c['line_status']==True),255*(c['line_status']==False)),2), axis=1) 
-        #print(df_l)
+          c['y2']),(255*(c['line_status']==5),255*(c['line_status']==True),\
+                                   255*(c['line_status']==False)),2), axis=1)
         
 #        plt.figure(figsize=(13,7))
 #        plt.title('Box - %d' %(bn+1) )
@@ -713,7 +800,7 @@ def checker(image_path,A=-1,B=-1,X=-1,Y=-1):
     plt.close('all')
     return df_chars
 
-def analysis(image_path, df_chars):
+def analysis(image_path):
     
     df_chars = checker(image_path)
     
@@ -820,7 +907,16 @@ image_names1 = ['data/kk_images/document.jpg',
                 'data/kk_images/document4.jpg',
                 'data/kk_images/document5.jpg',
                 'data/kk_images/document6.jpg',
-                'data/kk_images/document7.jpg',]
+                'data/kk_images/document7.jpg']
+
+image_namesM = ['data/mods/document_c2.jpg',
+                'data/mods/document1_c2.jpg',
+                'data/mods/document2_c2.jpg',
+                'data/mods/document3_c2.jpg',
+                'data/mods/document4_c2.jpg',
+                'data/mods/document5_c2.jpg',
+                'data/mods/document6_c2.jpg',
+                'data/mods/document7_c2.jpg']
 
 df_all = pd.DataFrame()
 
@@ -831,9 +927,8 @@ for image in image_names1:
     print(image)
     start = time.time()
     image_path = image
-    image_name = image.split('/')[1].split('.jpg')[0]
-    df_chars = checker(image_path, 1,1,12,3)
-    df_tmp = pd.DataFrame([[image_name]+analysis(image_path, df_chars)+[time.time() - start]])
+    image_name = image.split('/')[2].split('.jpg')[0]
+    df_tmp = pd.DataFrame([[image_name]+analysis(image_path)+[time.time() - start]])
     df_all = pd.concat([df_all, df_tmp], axis = 0)
     t_list.append([time.time() - start])
 
@@ -845,6 +940,8 @@ df_final = pd.DataFrame()
 df_final['Intermediate Steps of Line'] = df_all.apply(lambda x : (x['line_det'] + x['char_det'] + x['exp_det'])/3, axis = 1 )
 df_final['Line Correctness Accuracy'] = df_all['Color_prediction'].copy()
 df_final['image_name'] = df_all.image_name.copy()
+
+df_final.describe()
 #%%
 import seaborn as sns
 df_final.plot(x='image_name', y=['Intermediate Steps of Line'], kind='bar')
